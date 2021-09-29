4c9954f2b8d3822ed71b7aa12917ee25edd27f2a
diegoavc12 <A00226803@itesm.mx>
2021-09-29 02:27:34 +0000

    Solved lab

--- labs/c-str-lib/static-linking.c
+++ labs/c-str-lib/static-linking.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(int argc, char** argv) {
 
-int main() {
-    return 0;
+    if(argv[1]==NULL || argv[2]==NULL || argv[3]==NULL){
+        printf("Error, missing parameters\n");
+        exit(1);
+    }
+
+    if(strcmp("-add",argv[1])==0){
+        mystradd(argv[2],argv[3]);
+    }else if(strcmp("-find",argv[1])==0){
+        mystrfind(argv[2], argv[3]);
+    }else{
+        printf("Error, invalid parameters\n");
+    }
 }
--- labs/c-str-lib/strlib.c
+++ labs/c-str-lib/strlib.c
@@ -1,13 +1,53 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int len=0;
+    while(str[len]!='\0'){
+        len+=1;
+    }
+    return len;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+
+    int i;
+    char* result=NULL;
+    int lenOrigin=mystrlen(origin);
+    printf("Initial Length: %d \n",lenOrigin);
+
+    int lenAddition=mystrlen(addition);
+    result=(char*)malloc(lenOrigin+lenAddition);
+    for(i=0;i<lenOrigin;i++){
+        result[i]=origin[i];
+    }
+    for(i=lenOrigin;i<(lenOrigin+lenAddition);i++){
+        result[i]=addition[i-lenOrigin];
+    }
+    printf("New String: %s \n",result);
+    printf("New Length: %d \n",mystrlen(result));
+    return result;
 }
 
+
 int mystrfind(char *origin, char *substr){
+    int i;
+    int count=0;
+    int indexFound=0;
+    for(i=0; i<mystrlen(origin);i++){
+        if(origin[i]==substr[count]){
+            count+=1;
+        }else{
+            count=0;
+            indexFound=i;
+        }
+        if(count==mystrlen(substr)){
+            printf("[%s] was found at [%d] position \n", substr,indexFound);
+            return 1;
+        }
+    }
+    printf("[%s] was not found \n",substr);
     return 0;
+    
 }
+
